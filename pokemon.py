43e65e68b3811d42222c04958ff7c16258b3bee7
uno9 <ws060m@gmail.com>
2021-01-27 06:56:41 +0000

    [change]add check the user input

--- pokemon.py
+++ pokemon.py
@@ -1,9 +1,9 @@
 # -*- cording:utf-8 -*-
 
-# random関数のモジュール
 import random
 from colorama import Fore, Back, Style
 
+# 変数セット
 hand = ('グー','チョキ','パー')
 result = ('> 勝ち！','> 負け...',"> あいこ")
 trainer = ('エリートトレーナー','ドクター','たんパンこぞう')
@@ -19,39 +19,39 @@ afterSerif = (
   'まだまだ しょうぶは これからだ！'
   )
 
-trInt = random.randint(0,2)
 
-# 変数と文字列の結合は、f文字列を使用する
+trInt = random.randint(0,2)
 print(f'{trainer[trInt]}が しょうぶを しかけてきた！')
 print(f'{Fore.CYAN} {trainer[trInt]}『{beforeSerif[trInt]}』{Fore.RESET}')
 
 
 print(f' > 0.{hand[0]} \n > 1.{hand[1]} \n > 2.{hand[2]}')
-
 user = input('あなたは どうする？：')
-uInt = int(user)
-cpInt = random.randint(0,2)
-
-print('じゃんけん　ポンッ！')
 
 
+# 入力チェック
+uInt = int(user) if user.isdecimal() else {print(f'{Fore.RED} {trainer[trInt]}『 数字を 入力しよう！』{Fore.RESET}'),exit()}
 if uInt < 3:
   print(f'あなたは {Fore.CYAN}{hand[uInt]}{Fore.RESET}を 出した')
 else:
-  print(f'{Fore.RED} > 1 から 3 の 数字を 入力して じゃんけん しよう！{Fore.RESET}')
+  print(f'{Fore.RED} {trainer[trInt]}『 0 から 2 の 数字を 入力しよう！』{Fore.RESET}')
   exit()
 
 
+print('じゃんけん　ポンッ！')
+cpInt = random.randint(0,2)
+
+# 判定
 if uInt - cpInt == 0:
   print(f'{trainer[trInt]}は {Fore.CYAN}{hand[cpInt]}{Fore.RESET}を 出した')
   print(f'{Fore.CYAN}{result[2]}{Fore.RESET}')
-  print(f'{Fore.CYAN} {trainer[trInt]}『{afterSerif[2]}』{Fore.RESET}')
-  
+  print(f'{Fore.CYAN} {trainer[trInt]}『{afterSerif[2]}』{Fore.RESET}')  
+
 elif uInt - cpInt == 1 or uInt - cpInt == 2:
   print(f'{trainer[trInt]}は {Fore.CYAN}{hand[cpInt]}{Fore.RESET}を 出した')
   print(f'{Fore.GREEN}{result[0]}')
   print(f'{trainer[trInt]}『{afterSerif[0]}』{Fore.RESET}')
-  
+
 else:
   print(f'{trainer[trInt]}は {Fore.CYAN}{hand[cpInt]}{Fore.RESET}を 出した')
   print(f'{Style.DIM} {Fore.RED}{result[1]}')
